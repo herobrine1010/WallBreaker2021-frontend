1f48b683103cf6f3b84b8f96fd3efb8421dc3bd9
Vector341 <1786816365@qq.com>
2021-07-07 22:52:11 +0800

    济人/组队详情/回答问题
    字数限制还有些问题,各问题没有分开(有点不太会)

---
+++ pages/jiren/answerQuestion.js
@@ -0,0 +1,99 @@
+// pages/jiren/anwserQuestion.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    maxlengthMessage: "",
+    questionItems: [
+      {
+        index: 1,
+        questionText: '这里是问题，xxxxx？'
+      },
+      {
+        index: 2,
+        questionText: '这里是问题，xxxxx？'
+      }
+
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  submitAnswer: function(e) {
+    var answerList = e.detail.value
+
+
+    //调试用
+    console.log("form的submit的数据",answerList)
+  },
+  isMaxlength: function(e) {
+    var textLength = e.detail.value.length
+    if (textLength==50)
+    {
+      this.setData({
+        maxlengthMessage: "字符数已达上限！"
+      })
+    }
+
+
+
+  }
+
+})
