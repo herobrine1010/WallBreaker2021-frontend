3b247897c89e89a74832c08bcb9cc772fa33e85e
Vector341 <1786816365@qq.com>
2021-09-07 21:27:59 +0800

    同德 我的失物招领
    滑动问题待解决

--- pages/tongde/my.js
+++ pages/tongde/my.js
@@ -5,6 +5,108 @@ Page({
    * 页面的初始数据
    */
   data: {
+    myLostFoundItemList:[
+      {
+      'id': '10', //跳转用
+      'labelContent': ['水杯','钱包'],
+      'title':'示例标题示例标题',
+      'type': 0,
+      'postingDate':'2021年6月21日',
+      'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+      'postingPic':'',
+      'closed': false
+      },
+      {
+        'id': '11', //跳转用
+        'labelContent': ['水杯','钱包'],
+        'title':'示例标题示例标题',
+        'type': 0,
+        'postingDate':'2021年6月21日',
+        'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+        'postingPic':'',
+        'closed': false
+      },
+      {
+        'id': '11', //跳转用
+        'labelContent': ['水杯','钱包'],
+        'title':'示例标题示例标题',
+        'type': 0,
+        'postingDate':'2021年6月21日',
+        'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+        'postingPic':'',
+        'closed': false
+      },
+      {
+        'id': '10', //跳转用
+        'labelContent': ['水杯','钱包'],
+        'title':'示例标题示例标题',
+        'type': 0,
+        'postingDate':'2021年6月21日',
+        'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+        'postingPic':'',
+        'closed': false
+        },
+        {
+          'id': '11', //跳转用
+          'labelContent': ['水杯','钱包'],
+          'title':'示例标题示例标题',
+          'type': 0,
+          'postingDate':'2021年6月21日',
+          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+          'postingPic':'',
+          'closed': false
+        },
+        {
+          'id': '11', //跳转用
+          'labelContent': ['水杯','钱包'],
+          'title':'示例标题示例标题',
+          'type': 0,
+          'postingDate':'2021年6月21日',
+          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+          'postingPic':'',
+          'closed': false
+        },
+        {
+          'id': '11', //跳转用
+          'labelContent': ['水杯','钱包'],
+          'title':'示例标题示例标题',
+          'type': 0,
+          'postingDate':'2021年6月21日',
+          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+          'postingPic':'',
+          'closed': false
+        },
+        {
+          'id': '10', //跳转用
+          'labelContent': ['水杯','钱包'],
+          'title':'示例标题示例标题',
+          'type': 0,
+          'postingDate':'2021年6月21日',
+          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+          'postingPic':'',
+          'closed': false
+          },
+          {
+            'id': '11', //跳转用
+            'labelContent': ['水杯','钱包'],
+            'title':'示例标题示例标题',
+            'type': 0,
+            'postingDate':'2021年6月21日',
+            'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+            'postingPic':'',
+            'closed': false
+          },
+          {
+            'id': '11', //跳转用
+            'labelContent': ['水杯','钱包'],
+            'title':'示例标题示例标题',
+            'type': 0,
+            'postingDate':'2021年6月21日',
+            'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+            'postingPic':'',
+            'closed': false
+          }
+    ]
 
   },
 
