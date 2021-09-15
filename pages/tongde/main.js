b5a21535441d3414258a05ec15ba2d18ef1b7cac
Vector341 <1786816365@qq.com>
2021-09-16 00:07:45 +0800

    失物招领主页图片不显示bug
    使用css background-image不能用本地路径 一定用网络图片https://打头的

--- pages/tongde/main.js
+++ pages/tongde/main.js
@@ -1,4 +1,5 @@
 // pages/tongde/main.js
+import {request} from "../../request/request.js";
 Page({
 
   /**
@@ -16,7 +17,7 @@ Page({
         'title':'Gucci口红',
         'publishTime':'1天前',
         'description':'在学校图书馆捡到一支Gucci的口红，没有用过多少，九成新，…',
-        'postingPic':'/static/icon/lost-test.png',
+        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
         'type': 0,       // 物品遗失0 or 失物寻主1 
         'closed': false
       },
@@ -25,7 +26,7 @@ Page({
         'title':'物品名称',
         'publishTime':'1天前',
         'description':'简要描述简要描述简要描述捡到东西的地方是哪里…',
-        'postingPic':'/static/icon/lost-test2.png'
+        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700'
       },
       
       {
@@ -33,28 +34,28 @@ Page({
         'title':'物品名称',
         'publishTime':'1天前',
         'description':'简要描述简要描述简要描述捡到东西的地方是哪里…',
-        'postingPic':'/static/icon/lost-test.png'
+        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700'
       },
       {
         'labelText':['雨伞','口红'],
         'title':'物品名称',
         'publishTime':'1天前',
         'description':'简要描述简要描述简要描述捡到东西的地方是哪里…',
-        'postingPic':'/static/icon/lost-test.png'
+        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700'
       },
       {
         'labelText':['雨伞','口红'],
         'title':'物品名称',
         'publishTime':'1天前',
         'description':'简要描述简要描述简要描述捡到东西的地方是哪里…',
-        'postingPic':'/static/icon/lost-test.png'
+        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700'
       },
       {
         'labelText':['雨伞','口红'],
         'title':'物品名称',
         'publishTime':'1天前',
         'description':'简要描述简要描述简要描述捡到东西的地方是哪里…',
-        'postingPic':'/static/icon/lost-test.png'
+        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700'
       },
     
     ]
@@ -64,7 +65,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    request({
+      url: '/lostfound/tongdeGetLostFound',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        // 'cookie':wx.getStorageSync("token")
+      },
+      method : 'GET',
+      data: {
+        'type': '1',
+        // 'labelId': '30',
+        // 'keyword': ''
+      }
+    }).then(res => {
+      console.log(res.data.data);
+    });
   },
 
   // 绑定在点击tab上函数，通过点击切换swiper
@@ -83,7 +98,7 @@ Page({
     // TODO 根据current不同的值发送不同的请求，获取不同的数据列表
     this.setData({
       tab: e.detail.current
-    })
+    });
 
   },
   // 滚动框的 滚动和回到最上事件：------------- -------------- ----------
