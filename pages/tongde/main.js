0e04697e292f6880d3e38badce81439a09effc60
Vector341 <1786816365@qq.com>
2021-09-22 16:40:50 +0800

    同德主页筛选框
    引入筛选框组件需要添加：
    1. wxml中的组件引入，包括labels属性，bind:change
    2. js中两个函数，处理点击弹出, change

--- pages/tongde/main.js
+++ pages/tongde/main.js
@@ -81,7 +81,119 @@ Page({
       },
 
     
-    ]
+    ],
+    // 所有类型标签列表, 暂时直接写好, 后期需要更改时发请求
+    labelList: [
+      {
+        "id": 26,
+        "createTime": "2021-08-20 00:13:57",
+        "updateTime": "2021-08-20 00:13:57",
+        "content": "水杯",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 27,
+        "createTime": "2021-08-20 00:16:04",
+        "updateTime": "2021-08-20 00:16:04",
+        "content": "雨伞",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 28,
+        "createTime": "2021-08-20 00:16:04",
+        "updateTime": "2021-08-20 00:16:04",
+        "content": "证件",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 29,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "耳机",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 30,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "钥匙",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 31,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "钱包",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 32,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "数码",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 33,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "衣服",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 34,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "眼镜",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 35,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "文具",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 36,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "书籍",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      },
+      {
+        "id": 37,
+        "createTime": "2021-08-20 00:16:05",
+        "updateTime": "2021-08-20 00:16:05",
+        "content": "其他",
+        "type": "tongde",
+        "deleted": false,
+        "selected": null
+      }
+    ],
+    selectedLabelList: []
   },
 
   /**
@@ -105,7 +217,16 @@ Page({
     requestPromise.then(res => console.log(res));
 
   },
-
+  clickConditionFilter: function(e) {
+    let selector = this.selectComponent('#dialog-label-selector');
+    selector.openClose(); //不用全局变量,即可弹出关闭dailog筛选标签
+    selector.setLabelsSelected();
+  },
+  labelChanged: function(e) {
+    console.log(e);
+    let selectedLabelList = e.detail;
+    this.setData({selectedLabelList})
+  },
   // 绑定在点击tab上函数，通过点击切换swiper
   changeItem: function(e) {
 
