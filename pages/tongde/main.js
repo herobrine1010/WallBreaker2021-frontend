0e262c825e63030c53ea405152a26f922f87c974
Vector341 <1786816365@qq.com>
2021-09-26 17:25:06 +0800

    失物招领主页接口
    标签切换和跳转
    卡片单色

--- pages/tongde/main.js
+++ pages/tongde/main.js
@@ -5,6 +5,7 @@
 request是包装了一层promise的wx.request
 */
 import {request} from "../../request/request.js";
+import {formatTime, getDateDiff} from "../../utils/util.js"
 // 定义函数编写请求参数：-----------------------------------------
 function setRequestData(keyword,labelId,type){
   /* 设置request的参数, type必选, labelId, keyword可选 */
@@ -20,9 +21,11 @@ function setRequestData(keyword,labelId,type){
   }
   return data;
 };
-function getLostFoundList(type=0, labelId='', keyword='') {
+function getLostFoundList(that, type=0, labelId='', keyword='') {
   /* jiren jishi通过Page外的函数发起请求 setData, 需要传入that参数, 这个页面把它写在Page内.  */
-  return request({
+  // that.setData({isRefresherOpen: true});
+  // that.setData({tongdeItemList: []})
+  request({
     url: '/lostfound/tongdeGetLostFound',
     header: {
       'content-type': 'application/x-www-form-urlencoded',
@@ -30,6 +33,17 @@ function getLostFoundList(type=0, labelId='', keyword='') {
     },
     method : 'GET',
     data: setRequestData(keyword, labelId, type)
+  }).then(res => {
+    console.log("获取列表请求", res);
+    let itemList = res.data.data.map(v => {
+      v.createTime = getDateDiff(v.createTime);
+      return v;
+    })
+
+    that.setData({
+      tongdeItemList: itemList,
+      isRefresherOpen: false
+    })
   });
 }
 Page({
@@ -42,47 +56,48 @@ Page({
     tabIndex: 0, // 当前swiper选中项目
     tab: 0, //当前tab对应的项目
     showGoTopButton:false,
+    isRefresherOpen: false,
     topNum:0,
     tongdeItemList:[
       {
-        'labelText':['雨伞'],
-        'title':'雨伞',
-        'publishTime':'1天前',
-        'description':'9.10在嘉定A108门口捡到雨伞',
-        'postingPic':'https://img-blog.csdnimg.cn/20210920143006670.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16',
+        'labelList':['雨伞'],
+        'name':'雨伞',
+        'createTime':'1天前',
+        'content':'9.10在嘉定A108门口捡到一把雨伞',
+        'firstPicUrl':'https://img-blog.csdnimg.cn/20210920143006670.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16',
         'type': 0,       // 物品遗失0 or 失物寻主1 
         'closed': false
       },
       {
-        'labelText':['耳机'],
-        'title':'黑色耳机',
-        'publishTime':'2天前',
-        'description':'9.9闭馆时在嘉定图书馆九楼饮水机旁捡到一个黑色插线耳机',
-        'postingPic':'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic3.58cdn.com.cn%2Fzhuanzh%2Fn_v2e55f7bb2c83d45979b5f82ab1aa8e80e.jpg%3Fw%3D750%26h%3D0&refer=http%3A%2F%2Fpic3.58cdn.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1634710111&t=75304e2c48d2587a3aaafc933811f57e'
+        'labelList':['耳机'],
+        'name':'黑色耳机',
+        'createTime':'2天前',
+        'content':'9.9闭馆时在嘉定图书馆九楼饮水机旁捡到一个黑色插线耳机',
+        'firstPicUrl':'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic3.58cdn.com.cn%2Fzhuanzh%2Fn_v2e55f7bb2c83d45979b5f82ab1aa8e80e.jpg%3Fw%3D750%26h%3D0&refer=http%3A%2F%2Fpic3.58cdn.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1634710111&t=75304e2c48d2587a3aaafc933811f57e'
       },
       
       {
-        'labelText':['书籍'],
-        'title':'机器学习算法书',
-        'publishTime':'1天前',
-        'description':'9.9晚上在嘉定博楼217捡到一本西瓜书',
-        'postingPic':'https://img-blog.csdnimg.cn/2021092014302085.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16'
+        'labelList':['书籍'],
+        'name':'机器学习算法书',
+        'createTime':'1天前',
+        'content':'9.9晚上在嘉定博楼217捡到一本西瓜书',
+        'firstPicUrl':'https://img-blog.csdnimg.cn/2021092014302085.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16'
       },
       {
-        'labelText':['文具'],
-        'title':'笔记本',
-        'publishTime':'1天前',
-        'description':'9.10下午5点左右在嘉定B楼301教室前排捡到一本笔记本',
-        'postingPic':'https://img-blog.csdnimg.cn/20210920143025753.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16',
+        'labelList':['文具'],
+        'name':'笔记本',
+        'createTime':'1天前',
+        'content':'9.10下午5点左右在嘉定B楼301教室前排捡到一本笔记本',
+        'firstPicUrl':'https://img-blog.csdnimg.cn/20210920143025753.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16',
         'type': 0,       // 物品遗失0 or 失物寻主1 
         'closed': false
       },
       {
-        'labelText':['证件'],
-        'title':'一卡通',
-        'publishTime':'1天前',
-        'description':'9.9中午在嘉定小食堂二楼捡到一个卡套，有一卡通和钥匙',
-        'postingPic':'https://img-blog.csdnimg.cn/20210920143013638.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16'
+        'labelList':['证件'],
+        'name':'一卡通',
+        'createTime':'1天前',
+        'content':'9.9中午在嘉定小食堂二楼捡到一个卡套，有一卡通和钥匙',
+        'firstPicUrl':'https://img-blog.csdnimg.cn/20210920143013638.jpg?x-oss-process=image/watermark,type_ZHJvaWRzYW5zZmFsbGJhY2s,shadow_50,text_Q1NETiBAYWFhc2h1b3c=,size_20,color_FFFFFF,t_70,g_se,x_16'
       },
 
     
@@ -205,43 +220,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // getLostFoundList(this, '0', undefined, undefined);
-    var requestPromise = request({
-      url: '/lostfound/tongdeGetLostFound',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'cookie':wx.getStorageSync("token")
-      },
-      method : 'GET',
-      data: {
-        labelId: 30,
-        type: 0
-      }
-    });
-    console.log(requestPromise);
-    requestPromise.then(res => console.log(res));
+    getLostFoundList(this);
 
   },
-  getLostFoundList: function(type=0, labelId='', keyword='') {
-    /* 发起请求, 刷新卡片列表 */
-    let data = res.data.data;
-    request({
-      url: '/lostfound/tongdeGetLostFound',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'cookie':wx.getStorageSync("token")
-      },
-      method : 'GET',
-      data: setRequestData(keyword, labelId, type)
-    }).then(res => {
-      console.log(res);
-      data = res.data.data;
-    })
-    this.setData({
-      tongdeItemList: data
-    });
-  },
-
   // ------------筛选框相关的函数-----------------
   clickConditionFilter: function(e) {
     // 弹出筛选框
@@ -257,23 +238,19 @@ Page({
   // -----------筛选框函数结束--------------
   // 绑定在点击tab上函数，通过点击切换swiper
   changeItem: function(e) {
-
     this.setData({
       tabIndex: e.currentTarget.dataset.item
-    },
-    getLostFoundList(this, '', '', this.data.tabIndex)
-    )
+    });
+    getLostFoundList(this, this.data.tab, '', '');
 
   },
   // 绑定在swiper上的函数，用来改变tab
   changeTab: function(e) {
     var current = e.detail.current;
-    console.log('current',current);
-    // TODO 根据current不同的值发送不同的请求，获取不同的数据列表
     this.setData({
       tab: e.detail.current
     });
-
+    getLostFoundList(this, this.data.tab, '', '');
   },
   // 滚动框的 滚动和回到最上事件：------------- -------------- ----------
   returnTop: function () {
@@ -291,6 +268,12 @@ Page({
     }else if(this.data.showGoTopButton){
       this.setData({showGoTopButton:false})
     }
+  },  
+  // 滚动框的 下拉刷新事件 pullDownRefresh------------- -------------- ----------
+  onRefresherRefresh:function(){
+    // 重新发送请求，包括此前筛选或者搜索数据：
+    getLostFoundList(this, this.data.tab);
+    
   },
   // 跳转：发起组队事件：------------- --------- ------ ------- --
   createNewPost:function(){
