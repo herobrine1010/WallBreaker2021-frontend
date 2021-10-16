16a4be4d58588d62e861c854f664e261ebe80c77
Vector341 <1786816365@qq.com>
2021-10-16 18:07:46 +0800

    我的失物招领 关闭失物招领
    在我的-我的失物招领-关闭失物招领和跳转失物招领详情

--- components/card-lost/card-my.js
+++ components/card-lost/card-my.js
@@ -1,4 +1,5 @@
 // components/card-lost/card-my.js
+import {request} from "../../request/request.js";
 Component({
   /**
    * 组件的属性列表
@@ -13,7 +14,8 @@ Component({
         description: '在学校图书馆门口捡到一只Gucci的口红，看起来还没有用过多少，九成新。文字长度超过两行需要自动截取，只保留两行文字',
         postingPic: 'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
         type: 0,       // 物品遗失0 or 失物寻主1
-        closed: false
+        closed: false,
+        id: 0
       }
     },
   },
@@ -22,13 +24,24 @@ Component({
    * 组件的初始数据
    */
   data: {
-    title: '示例标题示例标题示例标题',
+/*     title: '示例标题示例标题示例标题',
     postingDate: '2021年6月21日',
     labelText: ['口红', 'Gucci'],
     description: '在学校图书馆门口捡到一只Gucci的口红，看起来还没有用过多少，九成新。文字长度超过两行需要自动截取，只保留两行文字',
     postingPic: 'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
     type:  0,
-    closed: false,
+    closed: false, */
+
+    dialog:{
+      isDialogShow: false,
+      content:'该条信息关闭后不可重启，确定要关闭吗?',
+      hasInputBox:false,
+      tip:"",
+      cancelText:"取消",
+      okText:"确认",
+      isShowInfo:false,
+      /*tapOkEvent:"dialogTapOkForAcceptApplying"*/
+    },
   },
   lifetimes: {
     attached: function() {
@@ -41,6 +54,7 @@ Component({
         postingPic: card.postingPic,
         type:  card.type,
         closed: card.closed,
+        id: card.id
       });
     }
   },
@@ -49,12 +63,34 @@ Component({
    */
   methods: {
     tapClose: function() {
+      // this.triggerEvent('close',this.data.detail);
+      this.setData({
+        'dialog.isDialogShow':true,
+      });
+
+    },
+    tapOk: function() {
+      const that = this;
       this.setData({closed:true});
-      // this.triggerEvent('tapClose',this.data.detail);
+      request({
+        url: "/userLostFound/removeMyLostFound",
+        header: {
+          'content-type': 'application/json',
+          'cookie':wx.getStorageSync("token")
+        },
+        method : 'POST',
+
+        data: {
+          id: that.data.id
+        }
+      }).then(res => {
+        let data = res.data.data;
+        console.log(res)
+      })
     },
     navigateToDetail: function() {
       wx.navigateTo({
-        url: '/pages/tongde/detail',
+        url: '/pages/tongde/detail?lostfoundId='+this.data.id,
       })
     }
   }
--- pages/tongde/my.js
+++ pages/tongde/my.js
@@ -1,149 +1,61 @@
 // pages/tongde/my.js
+import {request} from "../../request/request.js";
+import {formatTime, getDateDiff} from "../../utils/util.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    myLostFoundItemList:[
-      {
-      'id': '10', //跳转用
-      'labelContent': ['水杯','钱包'],
-      'title':'示例标题示例标题',
-      'type': 0,
-      'postingDate':'2021年6月21日',
-      'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-      'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-      'closed': false
-      },
-      {
-        'id': '11', //跳转用
-        'labelContent': ['水杯','钱包'],
-        'title':'示例标题示例标题',
-        'type': 0,
-        'postingDate':'2021年6月21日',
-        'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-        'closed': false
-      },
-      {
-        'id': '11', //跳转用
-        'labelContent': ['水杯','钱包'],
-        'title':'示例标题示例标题',
-        'type': 0,
-        'postingDate':'2021年6月21日',
-        'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-        'closed': false
-      },
-      {
-        'id': '10', //跳转用
-        'labelContent': ['水杯','钱包'],
-        'title':'示例标题示例标题',
-        'type': 0,
-        'postingDate':'2021年6月21日',
-        'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-        'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-        'closed': false
-        },
-        {
-          'id': '11', //跳转用
-          'labelContent': ['水杯','钱包'],
-          'title':'示例标题示例标题',
-          'type': 0,
-          'postingDate':'2021年6月21日',
-          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-          'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-          'closed': false
-        },
-        {
-          'id': '11', //跳转用
-          'labelContent': ['水杯','钱包'],
-          'title':'示例标题示例标题',
-          'type': 0,
-          'postingDate':'2021年6月21日',
-          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-          'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-          'closed': false
-        },
-        {
-          'id': '11', //跳转用
-          'labelContent': ['水杯','钱包'],
-          'title':'示例标题示例标题',
-          'type': 0,
-          'postingDate':'2021年6月21日',
-          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-          'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-          'closed': false
-        },
-        {
-          'id': '10', //跳转用
-          'labelContent': ['水杯','钱包'],
-          'title':'示例标题示例标题',
-          'type': 0,
-          'postingDate':'2021年6月21日',
-          'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-          'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-          'closed': false
-          },
-          {
-            'id': '11', //跳转用
-            'labelContent': ['水杯','钱包'],
-            'title':'示例标题示例标题',
-            'type': 0,
-            'postingDate':'2021年6月21日',
-            'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-            'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-            'closed': false
-          },
-          {
-            'id': '11', //跳转用
-            'labelContent': ['水杯','钱包'],
-            'title':'示例标题示例标题',
-            'type': 0,
-            'postingDate':'2021年6月21日',
-            'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
-            'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
-            'closed': false
-          }
-    ],
-    dialog:{
-      isDialogShow: false,
-      content:'该条信息关闭后不可重启，确定要关闭吗?',
-      hasInputBox:false,
-      tip:"",
-      cancelText:"取消",
-      okText:"确认",
-      isShowInfo:false,
-      /*tapOkEvent:"dialogTapOkForAcceptApplying"*/
-    },
+     myLostFoundItemList:[
+      // {
+      // 'id': '10', //跳转用
+      // 'labelContent': ['水杯','钱包'],
+      // 'title':'示例标题示例标题',
+      // 'type': 0,
+      // 'postingDate':'2021年6月21日',
+      // 'description':'这是一段描述性文字，仅用于测试。这是一段用来测试省略号是否能正常显示的文字',
+      // 'postingPic':'https://assets.burberry.com/is/image/Burberryltd/109A4FC5-0F33-4E5B-8488-11626C39FB3F.jpg?$BBY_V2_B_3x4$&wid=1278&hei=1700',
+      // 'closed': false
+      // }
+    ], 
 
-  },
-  tapClose:function(e){
-    //关闭帖子，提示是否确认关闭
-    var that = this;
-    that.setData({
-      'dialog.isDialogShow':true,
-      
-    })
-  },
-  tapOk:function(e){
-    var that = this;
-    console.log(e)
-    wx.showToast({
-      title: '成功关闭帖子',
-    })
-    //TODO 根据帖子的id显示关闭按钮
-    that.setData({
-      'myLostFoundItemList[0].closed':true
-    },that.onLoad());
-    this.onLoad();
-    
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const that = this;
+    request({
+      url: "/userLostFound/getMYLostFound",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie':wx.getStorageSync("token")
+      },
+      method : 'GET',
+    }).then(res => {
+      let data = res.data.data;
+      console.log("我的失物招领返回数据：",data);
+      let myLostFoundItemList = data.map(v => {
+        // 分割日期字符串"2021-5-23 12:23:34" 再连接成"2021年5月23号"
+        let dateList = v.createTime.split(/-|\s/,3);
+        let dateString = `${dateList[0]}年${dateList[1]}月${dateList[2]}日`;
+        console.log(dateString);
+        return {
+          'id': v.id, //跳转用
+          'title':  v.name,
+          'type': v.type,
+          'postingDate': dateString,
+          'description': v.content,
+          'postingPic': v.firstPicUrl,
+          'closed': v.closed,
+          'labelContent':[],
+          'closed': v.status // 帖子是否关闭 0--未关闭 1--关闭
+        }
+      });
+      that.setData({
+        myLostFoundItemList,
+      });
+    })
   },
 })
