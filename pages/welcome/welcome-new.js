5447bf9dd83ab314ae07c147c47cbe4347e21805
Vector341 <1786816365@qq.com>
2022-01-23 16:29:49 +0800

    邮箱验证
    跳转邮箱，点击链接 完成验证 回到小程序

--- pages/welcome/welcome-new.js
+++ pages/welcome/welcome-new.js
@@ -10,9 +10,17 @@ Page({
     countDownText : '获取验证码',
     hasMail : false,
     isVertifyBolck : false,
-
+    openid: '',
+    isSendMail: false
   },
-
+  onLoad: function (options) {
+    let openid = options.openid;
+    this.setData({
+      openid
+    }, this.checkMailIsClicked);
+    // this.checkMailIsClicked()
+    
+  },
   onInputMail(e){
     let mailStr = e.detail.value;
     if(mailStr.length > 0){
@@ -88,48 +96,74 @@ Page({
       });
     }
   },
-
+  getPhoneNumber (e) {
+    console.log(e.detail.code)
+  },
   onSubmit(e){
 
     // 1.完成邮箱认证；2.获取用户头像、昵称信息
     // {mail: "222", vertifyNum: "456"}
-    let mail = e.detail.value.mail ;
-    let mailLength = mail.length;
-    mail +=  '@tongji.edu.cn';
-    let vertifyNum = e.detail.value.vertifyNum;
-    let vertifyLength = vertifyNum.length;
-    if(mailLength>0 && vertifyLength>0){
+    // 进行邮箱验证；
+    // 判断手机号是否授权
+    // 返回相应的错误信息
+    const sid = e.detail.value.sid; // string
+    const openid = this.data.openid;
+    if(sid.length > 0){
+      // 发送验证邮箱
+      wx.showLoading({
+        title: '请稍等...',
+      })
       request({
-        url : '/verificationCode/check',
+        url : '/user/getMailOnRegister/' + openid + '/' + sid,
         method : 'GET',
         header: {
           "content-type": 'application/x-www-form-urlencoded'
         },
-        data:{
-          mail ,
-          code : vertifyNum 
-        }
       }).then(res => {
-        console.log(res);
+        wx.hideLoading({});
+        if(res.data.success) {
+          wx.showModal({
+            content: '已发送邮件，请前往邮箱完成注册',
+            showCancel: false,
+          })
+          this.setData({
+            isSendMail: true
+          })
+        } else {
+          wx.showModal({
+            title: '出错了',
+            content: `${res.data.msg.split(':')[0]}`,
+            showCancel: false,
+          })
+        }
       })
-      // 进行邮箱验证；
-      // 判断手机号是否授权
-      // 返回相应的错误信息
-
-
     }else{
-      if(mailLength <= 0){
-        this.setData({
-          isMailWarnShow : true
-        })
-      }
-      if(vertifyLength <= 0){
+      if(sid.length <= 0){
         this.setData({
           isVertifyWarnShow : true
         })
       }
-    } 
-    
+    }
 
+  },
+
+  checkMailIsClicked() {
+    if(this.data.isSendMail!=true) return;
+    request({
+      url: '/user/checkMailVerified/' + this.data.openid,
+    }).then(res => {
+      if(res.success) {
+        // 已点击流程
+        wx.redirectTo({
+          url: 'pages/main/jishi',
+        })
+      } else {
+        // 未点击流程
+        wx.showModal({
+          content: '您还没有验证邮箱, 请前往验证邮箱',
+          showCancel: false,
+        })
+      }
+    })
   }
 })
