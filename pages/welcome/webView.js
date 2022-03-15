15a24c5c2e0ab840dda02e7189cfdf54f681ffab
Zekun Su <18702139357@163.com>
2022-03-15 21:23:33 +0800

    域名更改 ： 统一身份认证页

--- pages/welcome/webView.js
+++ pages/welcome/webView.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tongjiUrl : "https://jixingyun.tongji.edu.cn/nidp/app/login?id=Login&sid=0&option=credential&sid=0&target=https%3A%2F%2Fids.tongji.edu.cn%3A8443%2Fnidp%2Foauth%2Fnam%2Fauthz%3Fscope%3Dprofile%26response_type%3Dcode%26redirect_uri%3Dhttps%3A%2F%2Fjixingyun.tongji.edu.cn%2Fapi%2Foauth%2Fcallback%26client_id%3Da5e7f46c-5fa3-4e15-b2e2-d9defd7476ea"
+    // tongjiUrl : "https://jixingyun.tongji.edu.cn/nidp/app/login?id=Login&sid=0&option=credential&sid=0&target=https%3A%2F%2Fids.tongji.edu.cn%3A8443%2Fnidp%2Foauth%2Fnam%2Fauthz%3Fscope%3Dprofile%26response_type%3Dcode%26redirect_uri%3Dhttps%3A%2F%2Fjixingyun.tongji.edu.cn%2Fapi%2Foauth%2Fcallback%26client_id%3Da5e7f46c-5fa3-4e15-b2e2-d9defd7476ea"
+    tongjiUrl : "https://tongji-poby.sparkxyf.com/nidp/app/login?id=Login&sid=0&option=credential&sid=0&target=https%3A%2F%2Fids.tongji.edu.cn%3A8443%2Fnidp%2Foauth%2Fnam%2Fauthz%3Fscope%3Dprofile%26response_type%3Dcode%26redirect_uri%3Dhttps%3A%2F%2Fjixingyun.tongji.edu.cn%2Fapi%2Foauth%2Fcallback%26client_id%3Da5e7f46c-5fa3-4e15-b2e2-d9defd7476ea"
   },
 
   /**
