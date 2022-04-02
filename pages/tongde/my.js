03227e0ca05d570a42eacfbb8723e979170117f8
Vector341 <1786816365@qq.com>
2022-04-02 23:48:16 +0800

    feat: 我的失物招领分页接口

--- pages/tongde/my.js
+++ pages/tongde/my.js
@@ -2,6 +2,45 @@
 import {request} from "../../request/request.js";
 import {formatTime, getDateDiff} from "../../utils/util.js";
 const setHeight = require("../../behaviors/SetHeight.js")
+const app = getApp()
+
+async function requestWithPage(pageNo) {
+  return request({
+    url: "/userLostFound/getMyLostFoundWithPage",
+    header: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    method : 'GET',
+    data: {
+      pageNo: pageNo,
+      pageSize: 7
+    }
+  })
+}
+function dealRequestData(res) {
+  let data = res.data.data.records;
+  const pages = res.data.data.pages;
+  let myLostFoundItemList = data.map(v => {
+    // 分割日期字符串"2021-5-23 12:23:34" 再连接成"2021年5月23号"
+    let dateList = v.createTime.split(/-|\s/,3);
+    let dateString = `${dateList[0]}年${dateList[1]}月${dateList[2]}日`;
+    return {
+      'id': v.id, //跳转用
+      'title':  v.name,
+      'type': v.type,
+      'postingDate': dateString,
+      'description': v.content,
+      'postingPic': v.firstPicUrl,
+      'closed': v.closed,
+      'labelContent': v.labelList,
+      'closed': v.status, // 帖子是否关闭 0--未关闭 1--关闭
+      'ownerNotice': v.ownerNotice,
+      'isPublishedByMe': v.ownerUserId === app.globalData.token
+    }
+  });
+  return {myLostFoundItemList, pages};
+
+}
 Component({
   behaviors: [setHeight],
   /**
@@ -20,6 +59,7 @@ Component({
       // 'closed': false
       // }
     ], 
+    pageNo: 1,
 
   },
   /**
@@ -27,36 +67,28 @@ Component({
    */
   methods: {
   onLoad: function (options) {
-    request({
-      url: "/userLostFound/getMYLostFound",
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-         
-      },
-      method : 'GET',
-    }).then(res => {
-      let data = res.data.data;
-      let myLostFoundItemList = data.map(v => {
-        // 分割日期字符串"2021-5-23 12:23:34" 再连接成"2021年5月23号"
-        let dateList = v.createTime.split(/-|\s/,3);
-        let dateString = `${dateList[0]}年${dateList[1]}月${dateList[2]}日`;
-        return {
-          'id': v.id, //跳转用
-          'title':  v.name,
-          'type': v.type,
-          'postingDate': dateString,
-          'description': v.content,
-          'postingPic': v.firstPicUrl,
-          'closed': v.closed,
-          'labelContent': v.labelList,
-          'closed': v.status // 帖子是否关闭 0--未关闭 1--关闭
-        }
-      });
-      myLostFoundItemList.reverse();
+    requestWithPage(1)
+    .then(res => {
+
+      const {myLostFoundItemList} = dealRequestData(res);
       this.setData({
         myLostFoundItemList,
       });
     })
   },
+  async getNextPage() {
+    if(this.data.isLoading === true) return;
+    if(this.data.pageNo >= this.data.pages) return;
+
+    this.setData({isLoading: true})
+    const res = await requestWithPage(this.data.pageNo + 1);
+    const {myLostFoundItemList:nextPageData,pages} = dealRequestData(res);
+    this.setData({
+      myLostFoundItemList: this.data.myLostFoundItemList.concat(nextPageData),
+      pageNo: this.data.pageNo + 1,
+      pages
+    });
+    this.setData({isLoading: false})
+  }
   }
 })
